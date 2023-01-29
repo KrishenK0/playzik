4b0cbe8a04792904375a6c8e95571ed68684ad29
KrishenK <fox2581kriskenk@gmail.com>
2023-01-29 15:57:00 +0100

    change index.js

--- index.js
+++ index.js
@@ -23,5 +23,10 @@ app.use(sessions({
 // Route
 app.use('/api', api);
 
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+})
+
+
 const port = process.env.PORT || 8080;
 app.listen(port, () => console.log(`Listening on port : ${port}`));

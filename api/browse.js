e42104b7f98ed237732056e285075b4c533b7210
KrishenK <fox2581kriskenk@gmail.com>
2023-01-30 19:17:33 +0100

    fix browse function

--- api/browse.js
+++ api/browse.js
@@ -4,7 +4,7 @@ export default async function handler(req, res) {
     if (req.query.continuation) {
         reqBrowse(req.headers['x-goog-visitor-id'], req.query.continuation)
             .then(browse => res.json(browse))
-            .catch(error => res.status(500).json(error));
+            .catch(error => res.json(error));
     } else {
         reqBrowse(req.headers['x-goog-visitor-id'])
             .then(browse => {
@@ -12,8 +12,8 @@ export default async function handler(req, res) {
                     nextBrowse.items.unshift(...browse.items);
                     res.setHeader('Content-Type', 'application/json');
                     res.json(nextBrowse);
-                }).catch(error => res.status(500).json(error));
+                }).catch(error => res.status(error.code).json(error));
             })
-            .catch(error => res.status(500).json(error));
+            .catch(error => res.json(error));
     }
 }

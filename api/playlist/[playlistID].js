0ce8c48cea523d366595de8e3b50c14f7b9374e3
KrishenK <fox2581kriskenk@gmail.com>
2023-01-30 19:34:11 +0100

    fix typo +  variable instance error

--- api/playlist/[playlistID].js
+++ api/playlist/[playlistID].js
@@ -2,8 +2,8 @@ const { reqPlaylist } = require('../../lib/utils');
 
 export default function handler(req, res) {
     if (req.method === 'OPTIONS') return res.status(200).end();
-    if (req.query.videoID) {
-        reqPlaylist(req.headers['x-goog-visitor-id'], req.query.videoID)
+    if (req.query.playlistID) {
+        reqPlaylist(req.headers['x-goog-visitor-id'], req.query.playlistID)
             .then(content => res.json(content))
             .catch(error => res.status(400).json(error))
     } else res.status(400).end("Internal Server Error");

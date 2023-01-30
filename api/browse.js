f48e6119043e0fc7cc53cb9a1b4e2036b7a3c344
KrishenK <fox2581kriskenk@gmail.com>
2023-01-30 18:41:23 +0100

    fix browse function

--- api/browse.js
+++ api/browse.js
@@ -2,13 +2,13 @@ const { reqBrowse } = require('../lib/utils');
 
 export default async function handler(req, res) {
     if (req.query.continuation) {
-        reqBrowse(visitorId, req.query.continuation)
+        reqBrowse(req.headers['x-goog-visitor-id'], req.query.continuation)
             .then(browse => res.json(browse))
             .catch(error => res.status(500).json(error));
     } else {
-        reqBrowse(visitorId)
+        reqBrowse(req.headers['x-goog-visitor-id'])
             .then(browse => {
-                reqBrowse(visitorId, browse.continuation).then(nextBrowse => {
+                reqBrowse(req.headers['x-goog-visitor-id'], browse.continuation).then(nextBrowse => {
                     nextBrowse.items.unshift(...browse.items);
                     res.setHeader('Content-Type', 'application/json');
                     res.json(nextBrowse);

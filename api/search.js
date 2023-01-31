151057f95d2457c587637e8794cc7c6d3a51f36f
KrishenK <fox2581kriskenk@gmail.com>
2023-01-31 14:44:58 +0100

    change ERROR code

--- api/search.js
+++ api/search.js
@@ -2,13 +2,14 @@ const { reqSearch, reqSuggestion } = require('../lib/utils');
 
 export default function handler(req, res) {
     if (req.method === 'OPTIONS') return res.status(200).end();
-    if (req.query.suggestion)
+    console.log(req.query);
+    if (req.query.suggestion || req.query.suggestion == '')
         reqSuggestion(req.headers['x-goog-visitor-id'], req.query.suggestion ?? '')
             .then(content => res.json(content))
-            .catch(error => res.status(200).json(error))
-    else if (req.query.q)
+            .catch(error => res.status(error.code).json(error))
+    else if (req.query.q || req.query.suggestion == '')
         reqSearch(req.headers['x-goog-visitor-id'], req.query.q)
             .then(content => res.json(content))
-            .catch(error => res.status(400).json(error))
-    else res.status(400).end("Internal Server Error");
+            .catch(error => res.status(error.code).json(error))
+    else res.status(500).end("Internal Server Error");
 }

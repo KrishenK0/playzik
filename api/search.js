7899b284bd9dfeb43c234335f3c0fee1619d87ce
KrishenK <fox2581kriskenk@gmail.com>
2023-01-31 14:36:18 +0100

    add suggestion function

--- api/search.js
+++ api/search.js
@@ -1,10 +1,14 @@
-const { reqSearch } = require('../lib/utils');
+const { reqSearch, reqSuggestion } = require('../lib/utils');
 
 export default function handler(req, res) {
     if (req.method === 'OPTIONS') return res.status(200).end();
-    if (req.query.q) {
+    if (req.query.suggestion)
+        reqSuggestion(req.headers['x-goog-visitor-id'], req.query.suggestion ?? '')
+            .then(content => res.json(content))
+            .catch(error => res.status(200).json(error))
+    else if (req.query.q)
         reqSearch(req.headers['x-goog-visitor-id'], req.query.q)
             .then(content => res.json(content))
             .catch(error => res.status(400).json(error))
-    } else res.status(400).end("Internal Server Error");
+    else res.status(400).end("Internal Server Error");
 }

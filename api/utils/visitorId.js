b164066cb6dcc334ab796fa3938d3753bd7be41a
KrishenK <fox2581kriskenk@gmail.com>
2023-02-03 17:32:29 +0100

    add cache + remove unusual package +add command

--- api/utils/visitorId.js
+++ api/utils/visitorId.js
@@ -2,6 +2,7 @@ const { get_visitor_id } = require('../../lib/utils');
 
 export default function handler(req, res) {
     if (req.method === 'OPTIONS') return res.status(200).end();
+    res.setHeader('Cache-Control', 's-maxage=3600, stale-while-revalidate');
     get_visitor_id()
         .then(browse => res.json(browse))
         .catch(error => res.status(500).json(error));
